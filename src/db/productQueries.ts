e41f768571204cd410ff503d86f255071fec6ee4
Dino Tutic <dino.tutic96@gmail.com>
2024-11-23 00:58:04 +0100

    backup before cleaning up code

---
+++ src/db/productQueries.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { z } from "zod";
+import prisma from "./prisma";
+
+// z.object({
+//   name: z.string().min(1),
+//   description: z.string().min(1),
+//   priceInCents: z.coerce.number().int().min(1),
+//   stock: z.coerce.number().int().min(1),
+//   isAvailable: z.boolean(),
+//   image: z.string().min(1),
+// });
+
+export async function getAllProducts() {
+  const products = await prisma.product.findMany();
+  return products;
+}
+
+export async function addProduct(formData: FormData) {
+  console.log("Logging form data", formData);
+  console.log("category: ", formData.get("category"));
+  console.log("style: ", formData.get("style"));
+}
+
+export async function getCategories() {
+  const data = await prisma.category.findMany();
+  const categories = data.map((category) => category.name);
+  return categories;
+}
+
+export async function getStyles() {
+  const data = await prisma.style.findMany();
+  const styles = data.map((style) => style.name);
+  return styles;
+}
