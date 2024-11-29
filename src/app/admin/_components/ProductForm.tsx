3757b171256a3912ac7cac5155e189ffef8af687
Dino Tutic <dino.tutic96@gmail.com>
2024-11-29 16:11:27 +0100

    blocking files larger than 5mb and bodysizelimit 30mb

--- src/app/admin/_components/ProductForm.tsx
+++ src/app/admin/_components/ProductForm.tsx
@@ -4,17 +4,87 @@ import { formatCurrency } from "@/app/lib/formatters";
 import { addProduct } from "@/db/productQueries";
 import { useState } from "react";
 
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  priceInCents: number;
+  stock: number;
+  isAvailable: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  categoryId: number;
+  styleId: number;
+  images: { url: string }[];
+};
 export default function ProductForm({
   categories,
   styles,
+  product,
 }: {
   categories: string[];
   styles: string[];
+  product?: Product;
 }) {
+  const [priceInEuros, setPriceInEuros] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
   const [priceInCents, setPriceInCents] = useState<number>(0);
+  const [stock, setStock] = useState<number>(0);
+  const [isAvailable, setIsAvailable] = useState<boolean>(true);
+  const [categoryId, setCategoryId] = useState<number>(0);
+  const [styleId, setStyleId] = useState<number>(0);
+  const [images, setImages] = useState<File[]>([]);
+
+  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+    setPriceInEuros(value);
+    setPriceInCents(value);
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const maxSize = 5 * 1024 * 1024; // 5 MB
+    if (e.target.files) {
+      const filesArray = Array.from(e.target.files);
+      const validFiles = filesArray.filter((file) => file.size <= maxSize);
+
+      if (validFiles.length !== filesArray.length) {
+        alert("Some files exceed the 5MB limit and will not be uploaded");
+      }
+      setImages(validFiles);
+    }
+  };
+
+  const handleImagePreviews = () => {
+    return images.map((image) => {
+      return (
+        <img
+          key={image.name}
+          src={URL.createObjectURL(image)}
+          alt={image.name}
+          className="h-32 w-32 object- mb-2"
+        />
+      );
+    });
+  };
+
+  const handleSUbmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+
+    images.forEach((image) => {
+      formData.append("images", image);
+    });
+
+    if (product) {
+      // await updateProduct(product.id, formData);
+    } else {
+      await addProduct(formData);
+    }
+  };
 
   return (
-    <form action={addProduct} className="product-form flex flex-col gap-3">
+    <form onSubmit={handleSUbmit} className="product-form flex flex-col gap-3">
       <div className="flex flex-col">
         <label htmlFor="name" className="mb-1">
           Name
@@ -23,6 +93,8 @@ export default function ProductForm({
           type="text"
           id="name"
           name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           required
           className="border rounded-md py-1"
         ></input>
@@ -34,6 +106,8 @@ export default function ProductForm({
         <textarea
           id="description"
           name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
           required
           className="border rounded-md py-1"
         ></textarea>
@@ -46,13 +120,14 @@ export default function ProductForm({
           type="number"
           id="priceInCents"
           name="priceInCents"
-          onChange={(e) => setPriceInCents(Number(e.target.value))}
+          value={priceInCents}
+          onChange={handlePriceChange}
           required
           className="border rounded-md py-1"
         ></input>
       </div>
       <div>
-        <p>{formatCurrency(priceInCents || 0)}</p>
+        <p>{formatCurrency(priceInEuros || 0)}</p>
       </div>
       <div className="flex flex-col">
         <label htmlFor="stock" className="mb-1">
@@ -62,6 +137,8 @@ export default function ProductForm({
           type="number"
           id="stock"
           name="stock"
+          value={stock}
+          onChange={(e) => setStock(Number(e.target.value))}
           required
           className="border rounded-md py-1"
         ></input>
@@ -74,9 +151,9 @@ export default function ProductForm({
           type="checkbox"
           id="isAvailable"
           name="isAvailable"
-          value="true"
+          checked={isAvailable}
+          onChange={(e) => setIsAvailable(e.target.checked)}
           required
-          defaultChecked
           className="mx-4"
         ></input>
       </div>
@@ -88,11 +165,13 @@ export default function ProductForm({
           type="file"
           id="images"
           name="images"
+          onChange={handleImageChange}
           required
           multiple
           className="border rounded-md py-1"
         ></input>
       </div>
+      <div className="flex flex-wrap gap-2 mt-2">{handleImagePreviews()}</div>
       <div className="flex flex-col">
         <label htmlFor="category" className="mb-1">
           Category
@@ -101,7 +180,8 @@ export default function ProductForm({
           id="category"
           name="category"
           required
-          defaultValue=""
+          value={categoryId}
+          onChange={(e) => setCategoryId(Number(e.target.value))}
           className="border rounded-md py-1"
         >
           <option value="" disabled>
@@ -123,7 +203,8 @@ export default function ProductForm({
         <select
           id="style"
           name="style"
-          defaultValue=""
+          value={styleId}
+          onChange={(e) => setStyleId(Number(e.target.value))}
           required
           className="border rounded-md py-1"
         >
@@ -141,7 +222,7 @@ export default function ProductForm({
       </div>
       <div className="flex items-center">
         <button className="border rounded-2xl p-4 bg-secondaryBackground text-secondaryText">
-          Add Product
+          {product ? "Save" : "Add Product"}
         </button>
       </div>
     </form>
