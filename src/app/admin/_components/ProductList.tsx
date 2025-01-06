9e5d3cb04f8b5684385900e6736743fcff0ad768
Dino Tutic <dino.tutic96@gmail.com>
2025-01-06 03:32:55 +0100

    added filtering

--- src/app/admin/_components/ProductList.tsx
+++ src/app/admin/_components/ProductList.tsx
@@ -19,15 +19,39 @@ type Product = {
   styleId: number;
 };
 
-export default function ProductList({ products }: { products: Product[] }) {
+export default function ProductList({
+  products,
+  categories,
+  styles,
+}: {
+  products: Product[];
+  categories: { id: number; name: string }[];
+  styles: { id: number; name: string }[];
+}) {
   const sizes = ["XS", "S", "M", "L", "XL"];
   const [filter, setFilter] = useState<string>("");
   const [sort, setSort] = useState<string>("");
-
+  const [styleFilter, setStyleFilter] = useState<string | null>(null);
+  const [categoryFilter, setCategoryFilter] = useState<string | null>(null);
+  const [availabilityFilter, setAvailabilityFilter] = useState<boolean | null>(
+    null
+  );
   const filteredProducts = products.filter((product) => {
+    const matchesNameFilter = product.name
+      .toLowerCase()
+      .includes(filter.toLowerCase());
+    const matchesAvailability =
+      availabilityFilter === null || product.isAvailable === availabilityFilter;
+    const matchesCategory =
+      categoryFilter === null || product.category.name === categoryFilter;
+    const matchesStyle =
+      styleFilter === null || product.style.name === styleFilter;
+
     return (
-      product.name.toLowerCase().includes(filter.toLowerCase()) ||
-      product.description.toLowerCase().includes(filter.toLowerCase())
+      matchesNameFilter &&
+      matchesAvailability &&
+      matchesCategory &&
+      matchesStyle
     );
   });
 
@@ -47,25 +71,71 @@ export default function ProductList({ products }: { products: Product[] }) {
 
   return (
     <div className="overflow-x-auto">
-      <div className="flex justify-between mb-4">
-        <input
-          type="text"
-          placeholder="Filter by name or description"
-          value={filter}
-          onChange={(e) => setFilter(e.target.value)}
-          className="border p-2 rounded"
-        />
-        <select
-          value={sort}
-          onChange={(e) => setSort(e.target.value)}
-          className="border p-2 rounded"
-        >
-          <option value="">Sort by</option>
-          <option value="priceAsc">Price: Low to High</option>
-          <option value="priceDesc">Price: High to Low</option>
-          <option value="nameAsc">Name: A to Z</option>
-          <option value="nameDesc">Name: Z to A</option>
-        </select>
+      <div className="flex mb-4 justify-between">
+        <h1 className="text-xl ml-4 text-gray-500">Filters:</h1>
+        <div className="flex gap-4">
+          <input
+            type="text"
+            placeholder="Filter by name"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            className="border p-2 rounded-xl"
+          />
+          <select
+            value={sort}
+            onChange={(e) => setSort(e.target.value)}
+            className="border p-2 rounded-xl"
+          >
+            <option value="">Sort by</option>
+            <option value="priceAsc">Price: Low to High</option>
+            <option value="priceDesc">Price: High to Low</option>
+            <option value="nameAsc">Name: A to Z</option>
+            <option value="nameDesc">Name: Z to A</option>
+          </select>
+          <select
+            value={categoryFilter ?? ""}
+            onChange={(e) =>
+              setCategoryFilter(e.target.value === "" ? null : e.target.value)
+            }
+            className="border p-2 rounded-xl"
+          >
+            <option value="">All categories</option>
+            {categories.map((category) => (
+              <option key={category.name} value={category.name}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+          <select
+            value={styleFilter ?? ""}
+            onChange={(e) =>
+              setStyleFilter(e.target.value === "" ? null : e.target.value)
+            }
+            className="border p-2 rounded-xl"
+          >
+            <option value="">All styles</option>
+            {styles.map((style) => (
+              <option key={style.name} value={style.name}>
+                {style.name}
+              </option>
+            ))}
+          </select>
+          <select
+            value={
+              availabilityFilter !== null ? availabilityFilter.toString() : ""
+            }
+            onChange={(e) =>
+              setAvailabilityFilter(
+                e.target.value === "" ? null : e.target.value === "true"
+              )
+            }
+            className="border p-2 rounded-xl"
+          >
+            <option value="">Available Y/N</option>
+            <option value="true">Available</option>
+            <option value="false">Not Available</option>
+          </select>
+        </div>
       </div>
       <table className="min-w-full bg-white border text-center">
         <thead>
@@ -73,7 +143,7 @@ export default function ProductList({ products }: { products: Product[] }) {
             <th className="py-2 px-4 border-b">ID</th>
             <th className="py-2 px-4 border-b">Images</th>
             <th className="py-2 px-4 border-b">Name</th>
-            <th className="py-2 px-4 border-b">Description</th>
+            <th className="py-2 px-4 border-b">Available</th>
             <th className="py-2 px-4 border-b">Price</th>
             <th className="py-2 px-4 border-b">Stock</th>
             <th className="py-2 px-4 border-b">Category</th>
@@ -90,14 +160,16 @@ export default function ProductList({ products }: { products: Product[] }) {
                   <img
                     src={product.images[0].url}
                     alt={product.name}
-                    className="h-32 w-32 object-cover mb-2"
+                    className="h-16 w-16 object-cover mb-2"
                   />
                 </td>
               ) : (
                 <td className="py-2 px-4 border-b>">No image</td>
               )}
               <td className="py-2 px-4 border-b">{product.name}</td>
-              <td className="py-2 px-4 border-b">{product.description}</td>
+              <td className="py-2 px-4 border-b">
+                {product.isAvailable ? "Y" : "N"}
+              </td>
               <td className="py-2 px-4 border-b">
                 {(product.priceInCents / 100).toFixed(2)}€
               </td>
