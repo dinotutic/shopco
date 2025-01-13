25baaccd30b2b0e1dff71830419c59fd16cbed17
Dino Tutic <dino.tutic96@gmail.com>
2025-01-13 15:33:17 +0100

    customers page and customers list progress

---
+++ src/app/admin/customers/[customerId]/page.tsx
@@ -0,0 +1,34 @@
+import { getUserById } from "@/db/userQueries";
+import PageHeader from "../../_components/PageHeader";
+import UserDetail from "../../_components/UserDetail";
+import { getPurchasesByUserId, getSaleStatsForUser } from "@/db/saleQueries";
+import { getReviewByUser } from "@/db/reviewQueries";
+
+type UserPageProps = {
+  params: { customerId: string };
+};
+
+const CustomerPage = async ({ params }: UserPageProps) => {
+  const { customerId } = await params;
+  const customerIdNum = Number(customerId);
+  const user = await getUserById(customerIdNum);
+  const purchases = await getPurchasesByUserId(customerIdNum);
+  const salesStats = await getSaleStatsForUser(customerIdNum);
+  const reviews = await getReviewByUser(customerIdNum);
+  if (!user) {
+    return <div>User not found</div>;
+  }
+  return (
+    <div>
+      <PageHeader>User Page</PageHeader>
+      <UserDetail
+        user={user}
+        purchases={purchases}
+        salesStats={salesStats}
+        reviews={reviews}
+      />
+    </div>
+  );
+};
+
+export default CustomerPage;
