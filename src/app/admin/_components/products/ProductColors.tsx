dac9ec51a85de3d9b54637c2b65a9f1fb8764fe8
Dino Tutic <dino.tutic96@gmail.com>
2025-01-19 23:24:10 +0100

    cleaned up code and moved to different component

---
+++ src/app/admin/_components/products/ProductColors.tsx
@@ -0,0 +1,101 @@
+import { useRouter } from "next/navigation";
+import { Product, Color } from "../shared.types";
+
+interface ProductColorsProps {
+  isEditing: boolean;
+  product: Product;
+  colors: Color[];
+  availableColors: Color[];
+  setAvailableColors: React.Dispatch<React.SetStateAction<Color[]>>;
+}
+
+const ProductColors: React.FC<ProductColorsProps> = ({
+  isEditing,
+  product,
+  colors,
+  availableColors,
+  setAvailableColors,
+}) => {
+  const router = useRouter();
+
+  const handleColorClick = (color: Color) => {
+    setAvailableColors((prev) =>
+      prev.some((c) => c.id === color.id)
+        ? prev.filter((c) => c.id !== color.id)
+        : [...prev, color]
+    );
+  };
+
+  const handleColorLink = (color: Color) => {
+    if (availableColors.some((c) => c.id === color.id)) {
+      return router.push(`/admin/products/${product.id}/${color.id}`);
+    }
+  };
+
+  return (
+    <div className="flex flex-wrap gap-2">
+      {colors.map((color) => (
+        <ColorItem
+          key={color.id}
+          color={color}
+          isEditing={isEditing}
+          isSelected={availableColors.some((c) => c.id === color.id)}
+          handleColorClick={handleColorClick}
+          handleColorLink={handleColorLink}
+          availableColors={availableColors}
+        />
+      ))}
+    </div>
+  );
+};
+
+// Rendering the color item in ProductColors component
+
+interface ColorItemProps {
+  color: Color;
+  isEditing: boolean;
+  isSelected: boolean;
+  handleColorClick: (color: Color) => void;
+  handleColorLink: (color: Color) => void;
+  availableColors: Color[];
+}
+
+const ColorItem: React.FC<ColorItemProps> = ({
+  color,
+  isEditing,
+  isSelected,
+  handleColorClick,
+  handleColorLink,
+  availableColors,
+}) => {
+  const isAvailable = availableColors.some((c) => c.id === color.id);
+
+  const colorStyle = {
+    background:
+      color.name === "Colorful"
+        ? "linear-gradient(90deg, red, orange, yellow, green, blue, violet)"
+        : color.name,
+    opacity: isSelected ? 1 : 0.5,
+    cursor: isEditing || isAvailable ? "pointer" : "default",
+  };
+
+  return (
+    <div
+      className={`border-2 border-black-500 rounded-full ${
+        isSelected ? "border-black" : "border-transparent"
+      }`}
+    >
+      <div
+        className="w-8 h-8 rounded-full cursor-pointer border-2"
+        style={colorStyle}
+        onClick={
+          isEditing
+            ? () => handleColorClick(color)
+            : () => handleColorLink(color)
+        }
+      ></div>
+    </div>
+  );
+};
+
+export default ProductColors;
