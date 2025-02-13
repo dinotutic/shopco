e2c632cf1e43b6cf561e033430e1238cc782b7c4
Dino Tutic <dino.tutic96@gmail.com>
2025-02-13 20:06:02 +0100

    added brands bar

--- src/app/(frontend)/home/page.tsx
+++ src/app/(frontend)/home/page.tsx
@@ -1,9 +1,11 @@
+import Brands from "../_components/brands/brands";
 import Hero from "../_components/hero/Hero";
 
 const HomePage = () => {
   return (
     <div className="max-w-[1440px] border w-full overflow-hidden bg-gray_bg">
       <Hero />
+      <Brands />
     </div>
   );
 };
