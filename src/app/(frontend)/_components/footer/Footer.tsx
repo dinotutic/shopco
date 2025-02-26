c208198b2a0704cc2615abd1cf6944e47c9adef5
Dino Tutic <dino.tutic96@gmail.com>
2025-02-26 23:07:51 +0100

    added pt-10 to footer

--- src/app/(frontend)/_components/footer/Footer.tsx
+++ src/app/(frontend)/_components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import FooterLinks from "./FooterLinks";
 
 export const Footer = () => {
   return (
-    <section className="w-full px-20 bg-gray_bg">
+    <section className="w-full px-20 bg-gray_bg pt-10">
       <div className="flex items-center w-full border-b pb-4">
         <FooterLinks />
       </div>
