dfdfd42a6553bac88b5fbe93db482dedba8d534d
Dino Tutic <dino.tutic96@gmail.com>
2025-03-03 12:18:23 +0100

    .

--- src/app/(frontend)/_components/footer/Footer.tsx
+++ src/app/(frontend)/_components/footer/Footer.tsx
@@ -1,9 +1,10 @@
+import Newsletter from "../newsletter/Newsletter";
 import BellowFooter from "./BellowFooter";
 import FooterContent from "./FooterContent";
 
 export const Footer = () => {
   return (
-    <section className="w-full px-20 bg-gray_bg pt-10">
+    <section className="w-full max-w-[1440px] px-20 bg-gray_bg pt-10">
       <div className="flex items-center w-full border-b pb-4">
         <FooterContent />
       </div>
---
+++ src/app/(frontend)/shop/page.tsx
@@ -0,0 +1,9 @@
+const Shop = async () => {
+  return (
+    <section>
+      <h2>Casual</h2>
+    </section>
+  );
+};
+
+export default Shop;
